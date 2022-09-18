7a65ce1ba9b7c693ee66c30358581c6b207bf84b
Constantine Linnick <clinnick@recordlinker.com>
2022-09-18 16:31:36 +0700

    Fix entity with uuid

--- demo/src/main/kotlin/me/blzr/EntWithUuid.kt
+++ demo/src/main/kotlin/me/blzr/EntWithUuid.kt
@@ -22,7 +22,7 @@ interface EntWithUuidRepo : CrudRepository<EntWithUuid, UUID> {
     @Query(
         """
         select * from with_uuid
-        where :value is null or :value = nullable_uuid 
+        where :value is null or :value is not null and :value = nullable_uuid 
     """, nativeQuery = true
     )
     fun findByUuid(value: UUID?): Collection<EntWithUuid>
