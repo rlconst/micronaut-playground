a9cd5b64ebc8d8f6f143b477a54ffbe9440ba5b6
Constantine Linnick <clinnick@recordlinker.com>
2022-10-02 23:22:50 +0700

    After adding body annotations it works

--- generic-body/src/main/kotlin/me/blzr/BodyOperations.kt
+++ generic-body/src/main/kotlin/me/blzr/BodyOperations.kt
@@ -1,5 +1,6 @@
 package me.blzr
 
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.client.annotation.Client
 import jakarta.inject.Named
@@ -8,17 +9,17 @@ import jakarta.inject.Named
 @Named("client")
 interface BodyOperations {
     @Post("/map")
-    fun getBodyMap(body: Map<String, String>): Map<String, String>
+    fun getBodyMap(@Body body: Map<String, String>): Map<String, String>
 
     @Post("/list")
-    fun getBodyList(body: List<String>): List<String>
+    fun getBodyList(@Body body: List<String>): List<String>
 
     @Post("/customMap")
-    fun getBodyCustomMap(body: CustomMap): Map<String, String>?
+    fun getBodyCustomMap(@Body body: CustomMap): Map<String, String>?
 
     @Post("/customList")
-    fun getBodyCustomList(body: CustomList): List<String>?
+    fun getBodyCustomList(@Body body: CustomList): List<String>?
 
     @Post("/string")
-    fun getBodyString(body: String): String
+    fun getBodyString(@Body body: String): String
 }
